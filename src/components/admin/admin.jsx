7ca2f7e7bd6c9032539cf1c991b6567960b0e012
Thomas Kurz <thomaskurz.15@gmail.com>
2022-06-30 20:45:01 -0300

    minor changes

--- src/components/admin/admin.jsx
+++ src/components/admin/admin.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-import Navbar from "../navBar/navbar";
 
 
 function Admin() {
-    const user = "thomas"
+    const user = "Thomas"
     const payed = true
 
-    const categories = [
-        { id: 1, name: 'categorias', adress: '/admin/categorias' },
-        { id: 2, name: 'productos', adress: '/admin/productos' },
-        { id: 3, name: 'configuracion', adress: '/admin/configuracion' },
-        { id: 4, name: 'horarios', adress: '/admin/horarios' },
-        { id: 5, name: 'mi plan', adress: '/admin/mi-plan' },
-        { id: 6, name: 'carta', adress: '/' },
-    ]
+    
     
     return (
         <>
-            <Navbar categories={categories} />
-            <div className="container bg-slate-400 mx-auto m-2 p-2">
+            <div className="container bg-slate-400 mx-auto m-2 p-2 text-center">
             <h1>Beinvenido {user}!</h1>
             {payed ? 
                 <h2>Tu plan esta al dia</h2>
--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 
-function Navbar({ categories }) {
-    console.log(categories)
+function Navbar() {
+
+    const isAdmin = true
+
+
     return (
-        <nav className="container mx-auto bg-slate-500 ">
+        <nav className="container mx-auto bg-slate-500 m-3 p-2">
         <ul className="flex justify-evenly">
-            {
-                categories.map((cat) => <li className="m-1 p-1" key={cat.id}>{cat.name}</li> )
-            }
+            <Link to='/admin/categorias'><li className={isAdmin?"":"hidden" }>categorias</li></Link>
+            <Link to='/admin/productos'><li className={isAdmin?"":"hidden" }>productos</li></Link>
+            <Link to='/'><li>carta</li></Link>
+            <Link to='/reservas'><li>reservas</li></Link>
+            <Link to='/admin'><li>admin</li></Link>
+            <Link to='/admin/configuracion'><li className={isAdmin?"":"hidden" }>configuracion</li></Link>
+            <Link to='/admin/horarios'><li className={isAdmin?"":"hidden" }>horarios</li></Link>
+            <Link to='/admin/mi-plan'><li className={isAdmin?"":"hidden" }>mi-plan</li></Link>
         </ul>
         </nav>
   )
