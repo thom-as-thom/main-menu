d1cebc2809370eea789f813de0fe3adc5380872d
Thomas Kurz <thomaskurz.15@gmail.com>
2022-06-30 20:45:26 -0300

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import {BrowserRouter, Routes, Route, } from 'react-router-dom';
 
 import './App.css';
 import Admin from './components/admin/admin';
+import Products from './components/admin/products/products';
+import Navbar from './components/navBar/navbar';
+
+
 
 function App() {
-  return (
-    <Admin/>
+	return (
+		<BrowserRouter>
+		<Navbar />
+		<Routes>
+			<Route path='/admin' element={
+			<Admin/>
+			}>
+			</Route>
+				<Route path='/admin/productos' element={
+					<Products/>
+			}>
+					
+			</Route>
+		</Routes>
+		</BrowserRouter>
   );
 }
 
